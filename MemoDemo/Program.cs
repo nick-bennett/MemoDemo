79f60af617f9967108d097b9f9a370182df89953
Nick Bennett <nick@nickbenn.com>
2018-11-06 15:14:12 -0700

    Initial commit

---
+++ MemoDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace MemoDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = (args.Length > 0) ? int.Parse(args[0]) : 10;
+            Console.Write("Fibonacci({0}) = {1:N0}", n, Fibonacci(n));
+        }
+
+        private static BigInteger Fibonacci(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
